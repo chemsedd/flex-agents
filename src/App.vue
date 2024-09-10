114d12c2889b2dfd0339eb346dc040f00e3e1e73
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-09-11 00:43:25 +0100

    Add header with buttons

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,20 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <router-link :to="{ name: 'dashboard' }">Dashboard</router-link>
+  <header class="px-10 py-4 border-b flex justify-between items-center">
+    <div class="inline-flex gap-6 items-center">
+      <button class="btn">+</button>
+      <h2>LOGO</h2>
+    </div>
+
+    <div class="font-semibold">
+      <router-link :to="{ name: 'dashboard' }">Untitled project</router-link>
+    </div>
+
+    <div class="inline-flex gap-2 items-center">
+      <button class="btn-ghost">+</button>
+      <button class="btn-ghost">+</button>
+    </div>
   </header>
   <main>
     <router-view></router-view>
