e3da7df1a568654f297f3df998c7ddc79c3d90b7
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-09-11 02:25:28 +0100

    Add header with icons (feather icons)

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,35 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
+<script>
+import feather from 'feather-icons'
+
+export default {
+  mounted() {
+    feather.replace()
+  }
+}
 </script>
 
 <template>
   <header class="px-10 py-4 border-b flex justify-between items-center">
     <div class="inline-flex gap-6 items-center">
-      <button class="btn">+</button>
-      <h2>LOGO</h2>
+      <button class="btn-square">
+        <i data-feather="chevron-left"></i>
+      </button>
+      <h2 class="font-bold">FLEX<span class="text-indigo-500">A</span>.</h2>
     </div>
 
-    <div class="font-semibold">
-      <router-link :to="{ name: 'dashboard' }">Untitled project</router-link>
+    <div class="font-semibold inline-flex gap-2 items-center">
+      <span>Untitled project</span>
+      <i data-feather="chevron-down"></i>
     </div>
 
-    <div class="inline-flex gap-2 items-center">
-      <button class="btn-ghost">+</button>
-      <button class="btn-ghost">+</button>
+    <div class="inline-flex gap-2 items-center text-gray-900">
+      <button class="btn-ghost">
+        <i data-feather="box"></i>
+      </button>
+
+      <button class="btn-ghost">
+        <i data-feather="help-circle"></i>
+      </button>
     </div>
   </header>
   <main>
