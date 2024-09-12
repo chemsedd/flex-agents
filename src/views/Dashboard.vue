96fe018a3f518277d5ca0736dfb804390fcb6e89
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-09-12 19:02:31 +0100

    Add board header

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -110,9 +110,9 @@ export default {
           <i data-feather="search"></i>
           <span>Library</span>
         </button>
-        <div class="inline-flex gap-2">
-          <i data-feather="chevron-down"></i>
-          <i data-feather="minimize"></i>
+        <div class="inline-flex">
+          <button class="btn-ghost"><i data-feather="chevron-down"></i></button>
+          <button class="btn-ghost"><i data-feather="minimize"></i></button>
         </div>
       </div>
       <!-- Library items -->
@@ -128,7 +128,35 @@ export default {
       </ul>
     </div>
     <!-- Board -->
-    <main class="board">BOARD HERE</main>
+    <main class="board">
+      <div class="board-header">
+        <div class="inline-flex">
+          <!-- View controle -->
+          <button class="btn-ghost"><i data-feather="corner-up-left"></i></button>
+          <button class="btn-ghost"><i data-feather="corner-up-right"></i></button>
+          <button class="btn-ghost">
+            <span class="font-semibold">100%</span>
+            <i data-feather="maximize-2"></i>
+          </button>
+        </div>
+        <!-- Share options -->
+        <div class="inline-flex items-center gap-6">
+          <div class="inline-flex -space-x-2 text-purple-600">
+            <button class="btn-circle">J</button>
+            <button class="btn-circle">O</button>
+          </div>
+          <button class="btn-circle"><i data-feather="plus"></i></button>
+          <i data-feather="check-circle" class="stroke-green-500 stroke-[3]"></i>
+          <button class="btn shadow-sm">
+            <i data-feather="share"></i> <span class="font-semibold">Publish</span>
+          </button>
+          <button class="btn-ghost bg-gradient-to-br from-[#667EEA] to-[#764BA2] text-white">
+            <i data-feather="message-circle" class="stroke-white"></i>
+            <span class="font-semibold">Chat with my bot</span>
+          </button>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
@@ -137,8 +165,19 @@ export default {
   @apply flex flex-1 overflow-clip;
 }
 
+.inline-container {
+  @apply inline-flex gap-2 items-center justify-between text-sm font-medium;
+  @apply py-2 px-4 h-16;
+
+  & svg {
+    @apply h-4 w-4 text-gray-500;
+  }
+}
+
+// Sidebar
+
 .sidebar {
-  @apply p-2 flex flex-col overflow-clip border-r;
+  @apply flex flex-col overflow-clip border-r;
 
   & svg {
     @apply text-gray-500 h-5 w-5;
@@ -146,11 +185,12 @@ export default {
 }
 
 div.sidebar-header {
-  @apply py-2 inline-flex gap-2 items-center justify-between text-sm;
+  @apply inline-container border-b;
 }
 
 ul.sidebar-menu {
   @apply flex flex-col gap-y-2 overflow-scroll;
+  @apply px-2 py-2;
 
   & > li {
     @apply flex items-center justify-between gap-6 w-full;
@@ -159,7 +199,13 @@ ul.sidebar-menu {
   }
 }
 
+// Board
+
 .board {
-  @apply flex-1 flex items-center justify-center;
+  @apply flex-1 flex flex-col;
+}
+
+.board-header {
+  @apply inline-container px-6 border-b;
 }
 </style>
