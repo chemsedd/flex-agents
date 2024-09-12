dd4ef8788ec5e7bdb5c88b8484334e3518d21f38
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-09-12 23:50:30 +0100

    Fix router

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../views/Dashboard.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
-      path: '/dashboard',
+      path: '/',
       name: 'dashboard',
       component: Dashboard
     }
