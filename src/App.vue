44fa57632034a237b253063a40659fb15e88bc8c
Chems Eddine Senoussi <chemseddsen@gmail.com>
2024-09-14 00:55:45 +0100

    Refactor navbar with responsivness

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script>
 import feather from 'feather-icons'
+import Navbar from './components/Navbar.vue'
 import Dashboard from './views/Dashboard.vue'
 
 export default {
-  components: { Dashboard },
+  components: {
+    Navbar,
+    Dashboard
+  },
   mounted() {
     feather.replace()
   }
@@ -11,31 +15,8 @@ export default {
 </script>
 
 <template>
-  <!-- Header bar -->
-  <header class="px-10 py-4 border-b flex justify-between items-center">
-    <div class="inline-flex gap-6 items-center">
-      <button class="btn-square">
-        <i data-feather="chevron-left"></i>
-      </button>
-      <h2 class="font-bold">FLEX<span class="text-indigo-500">A</span>.</h2>
-    </div>
-
-    <div class="font-semibold inline-flex gap-2 items-center">
-      <span>Untitled project</span>
-      <i data-feather="chevron-down"></i>
-    </div>
-
-    <div class="inline-flex gap-2 items-center text-gray-900">
-      <button class="btn-ghost">
-        <i data-feather="box"></i>
-      </button>
-
-      <button class="btn-ghost">
-        <i data-feather="help-circle"></i>
-      </button>
-    </div>
-  </header>
-
+  <!-- Navbar -->
+  <Navbar />
   <!-- Page main body -->
   <div id="content" class="flex-1 overflow-hidden">
     <Dashboard />
@@ -44,17 +25,10 @@ export default {
 
 <style>
 #app {
-  /* Set the height to 100% of the viewport height */
-  height: 100dvh;
-  /* Use flexbox to create a column layout */
-  display: flex;
-  flex-direction: column;
+  @apply h-dvh flex flex-col;
 }
 
 #content {
-  /* Make the content stretch and take all available space */
-  flex: 1;
-  /* Allow scrolling within the content if necessary */
-  overflow: auto;
+  @apply flex-1;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="navbar">
+    <!--  -->
+    <div class="sm:hidden flex justify-end p-2">
+      <button class="btn-square" @click="toggleNavbar = !toggleNavbar">
+        <i data-feather="menu"></i>
+      </button>
+    </div>
+    <div :class="`navbar-container ${toggleNavbar ? 'flex' : 'hidden sm:flex'}`">
+      <!-- Logo section -->
+      <div class="inline-flex gap-6 items-center">
+        <button class="btn-square">
+          <i data-feather="chevron-left"></i>
+        </button>
+        <h2 class="font-bold">FLEX<span class="text-indigo-500">A</span>.</h2>
+      </div>
+      <!-- Change project -->
+      <div class="font-semibold inline-flex gap-2 items-center">
+        <span>Untitled project</span>
+        <i data-feather="chevron-down"></i>
+      </div>
+      <!-- Buttons section -->
+      <div class="inline-flex gap-2 items-center text-gray-900">
+        <button class="btn-ghost"><i data-feather="box"></i></button>
+        <button class="btn-ghost"><i data-feather="help-circle"></i></button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      toggleNavbar: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+header.navbar {
+  @apply border-b;
+
+  & > .navbar-container {
+    @apply flex-col justify-between items-center gap-4 px-10 py-4;
+    @apply sm:flex-row;
+  }
+}
+</style>
